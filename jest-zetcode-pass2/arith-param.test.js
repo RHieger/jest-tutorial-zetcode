19eb6eb85e68716bd999e26891c26762b90d7066
Robert Hieger <rhieger@gmail.com>
2020-09-26 15:37:56 -0400

    Add parameterized tests for multiplication

--- jest-zetcode-pass2/arith-param.test.js
+++ jest-zetcode-pass2/arith-param.test.js
@@ -19,3 +19,15 @@ test.each(
     expect( sub(a, b) ).toBe(expected);
   }
 );
+
+test.each(
+  [
+    [1, 1, 1],
+    [-1, 2, -2],
+    [2, 2, 4]
+  ]
+) (
+  '%i * %i equals %i', (a, b, expected) => {
+    expect( mul(a, b) ).toBe(expected);
+  }
+);
